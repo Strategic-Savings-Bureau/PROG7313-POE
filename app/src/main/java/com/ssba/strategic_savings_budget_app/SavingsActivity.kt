c687afab5d2b0cd6bb2a3402211d43f853321a0c
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2025-05-01 11:39:33 +0200

    Initialize SavingsActivity and add UI components - #36
    This commit refactors the `SavingsActivity` by:
    
    -   Initializing Firebase Authentication and App Database.
    -   Adding `ImageButton` for rewards and date range filtering.
    -   Adding a `TextView` for displaying a message when no savings goals are present.
    -   Adding `RecyclerView` to display savings goals.
    -   Implementing onClickListeners for `btnRewards` and `btnDateRangeFilter`.
    - Adding new strings, icons and colors to resources.
    - Updating layout for `activity_savings` to add the added components.

--- app/src/main/java/com/ssba/strategic_savings_budget_app/SavingsActivity.kt
+++ app/src/main/java/com/ssba/strategic_savings_budget_app/SavingsActivity.kt
@@ -2,8 +2,15 @@ package com.ssba.strategic_savings_budget_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.ssba.strategic_savings_budget_app.data.AppDatabase
 import com.ssba.strategic_savings_budget_app.databinding.ActivitySavingsBinding
 
 class SavingsActivity : AppCompatActivity() {
@@ -11,6 +18,17 @@ class SavingsActivity : AppCompatActivity() {
     // region Declarations
     // View Binding
     private lateinit var binding: ActivitySavingsBinding
+    // endregion
+
+    private lateinit var db: AppDatabase
+
+    private lateinit var auth: FirebaseAuth
+
+    // region View components
+    private lateinit var btnRewards: ImageButton
+    private lateinit var btnDateRangeFilter: ImageButton
+    private lateinit var tvNoSavingsGoals: TextView
+    private lateinit var rvSavingsGoals: RecyclerView
     // endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +43,33 @@ class SavingsActivity : AppCompatActivity() {
         // Highlight the Menu Item
         binding.bottomNav.selectedItemId = R.id.miSavings
 
+        // Initialise Firebase Authentication
+        auth = FirebaseAuth.getInstance()
+
+        // Initialise Database
+        db = AppDatabase.getInstance(this)
+
+        // region Initialise View Components
+        btnRewards = binding.btnRewards
+        btnDateRangeFilter = binding.btnDateFilter
+        tvNoSavingsGoals = binding.tvNoSavingsGoals
+        rvSavingsGoals = binding.rvSavingGoals
+        // endregion
+
         // Method to Set Up onClickListeners
         setupOnClickListeners()
     }
 
     private fun setupOnClickListeners() {
+
+        btnRewards.setOnClickListener {
+            Toast.makeText(this, "Rewards Coming Soon", Toast.LENGTH_SHORT).show()
+        }
+
+        btnDateRangeFilter.setOnClickListener {
+            Toast.makeText(this, "Date Range Filter Coming Soon", Toast.LENGTH_SHORT).show()
+        }
+
         // Set up Bottom Navigation View onClickListener
         binding.bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
