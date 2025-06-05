6b75ae9fce464bdc6bd6027e0e0cb3eefce4e2d7
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2025-06-05 22:04:46 +0200

    Added Firebase Firestore dependencies and SDK - #119

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
